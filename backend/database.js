b9d47acd14ff14ccf030292607f71674e48a44d1
nielnyu <ndc338@nyu.edu>
2023-07-23 20:44:49 -0400

    set up backend to sql post request connection

---
+++ backend/database.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql2")
+
+module.exports = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Ilgq87@9t1",
+    database: "EmailSender",
+})
